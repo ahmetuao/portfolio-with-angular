8b193fb2462ada1bd4246a6de16c49fd98a4a02c
Ahmet Yılmaz Ustaalioğlu <59164729+ahmetuao@users.noreply.github.com>
2021-09-30 18:57:13 +0300

    nine-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-with-angular';
+  showMe:boolean=false;
+  text:string= "Angular";
+  textProje: Array<any> = [
+    'Angular Proje 1', 'Angular Proje 2'
+  ];
+  constructor(){}
+
+  ngOnInit(){}
+
+  id:any = "hakkımda";
+  tabChange(ids:any){
+    this.id = ids;
+  }
+
+  idProje:any = "html";
+  tabChangeProje(x:any){
+    this.idProje = x;
+    console.log(this.idProje);
+  }
+
+  idHtmlProje:any = "proje1";
+  tabChangeHtmlProje(y:any){
+    this.
+    idHtmlProje = y;
+  }
+
+  idJs:any = "";
+  tabChangeJs(z:any){
+    this.idJs = z;
+  }
+
+  toggleTag(){
+    this.showMe=!this.showMe
+  }
+
+  js(text:string){
+    this.text = text
+  }
+
+  jsProje(textProje:Array<String>){
+    this.textProje = textProje;
+  }
+
+  
 }
